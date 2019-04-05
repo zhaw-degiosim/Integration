02b0f8ca11a9e7f08d418e58eba230fd99b1e083
nussbi <nussbi@LAPTOP-2C5RN20E>
2019-04-05 08:46:10 +0200

    Logging mit Log4j hinzugefügt

--- src/main/java/ch/tagcloud/www/TagCloudApplication.java
+++ src/main/java/ch/tagcloud/www/TagCloudApplication.java
@@ -5,14 +5,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 @SpringBootApplication
 public class TagCloudApplication extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(TagCloudApplication.class);
     }
+    
+    private static Logger logger = LogManager.getLogger(TagCloudApplication.class);
 
     public static void main(String[] args) throws Exception {
+    	logger.info("Starting Spring Boot application..");
         SpringApplication.run(TagCloudApplication.class, args);
     }
 }
