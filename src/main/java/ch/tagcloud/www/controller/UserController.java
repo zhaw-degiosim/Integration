c376c039ecc37dd757904d55cf9330c846818a51
nussbi <nussbi@LAPTOP-2C5RN20E>
2019-04-05 18:01:17 +0200

    LogEinträge für UserController

--- src/main/java/ch/tagcloud/www/controller/UserController.java
+++ src/main/java/ch/tagcloud/www/controller/UserController.java
@@ -1,7 +1,7 @@
 package ch.tagcloud.www.controller;
 
-import javax.validation.Valid;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -27,8 +27,12 @@ public class UserController {
 	@Autowired
     private UserValidator userValidator;
 	
+	private static Logger logger = LogManager.getLogger(UserController.class);
+	
 	@GetMapping(value = "/login")
     public String loginPage(Model model) {
+		
+		logger.info("Serve template: login");
  
         return "login";
     }
@@ -37,6 +41,8 @@ public class UserController {
     public String registerPage(Model model) {
     	
     	model.addAttribute("userForm", new User());
+    	
+    	logger.info("Serve template: register");
  
         return "register";
     }
@@ -44,15 +50,24 @@ public class UserController {
     @PostMapping(value = "/register")
     public String registerAddPage(@ModelAttribute("userForm") User userForm, BindingResult bindingResult) {
     	
+    	logger.info("Validate register form for user: " + userForm.getMail());
+    	
     	userValidator.validate(userForm, bindingResult);
 
         if (bindingResult.hasErrors()) {
+        	
+        	logger.info("Error in userForm. Return to register. User: " + userForm.getMail());
+        	
             return "register";
         }
     	
+        logger.info("Save User: " + userForm.getMail());
+        
     	userService.save(userForm);
     	
     	securityService.autoLogin(userForm.getMail(), userForm.getPasswordConfirm());
+    	
+    	logger.info("Autologin succesfull. redirect:/index?register=true");
  
     	return "redirect:/index?register=true";
     }
